e4f231babeac284ff378600c40450a28ce9b8119
Ploffin <plofftag@gmail.com>
2017-08-23 21:25:09 -0400

    bare functionality complete

---
+++ maketbl.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+all_csv_str=$( echo "*.csv" | sort )
+all_csv=( $all_csv_str )
+
+csv2html -o dptable.html -r --table 'class="dptable"' "${all_csv}"
+
+echo "document.write('$(cat dptable.html | sed 's/\x27/\&quot;/g' | tr '\n' ' ' )');" > dptable.js
