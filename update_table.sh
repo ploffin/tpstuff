6c2eb04630e16ccebab0825b0353192610c721f7
Ploffin <plofftag@gmail.com>
2017-08-26 01:34:37 +0200

    auto push added to update script

---
+++ update_table.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rm dp-spreadsheet.csv
+python get_packet.py 
+csv2html -o dptable.html -r --table 'class="dptable"' --tr 'class=tr-collapsed' dp-spreadsheet.csv
+
+echo "document.write('$(cat dptable.html | sed 's/\x27/\&quot;/g' | tr '\n' ' ' )');" > dptable.js
+
+git reset
+git add dptable.js
+git commit -m"update table"
+git push origin master
