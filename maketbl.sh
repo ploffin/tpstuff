4bfd3013f23664e6b242679f138c7381a8b94285
Ploffin <plofftag@gmail.com>
2017-08-26 01:55:51 +0200

    delete intermediate files

--- maketbl.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rm dp-spreadsheet.csv
-python get_packet.py 
-csv2html -o dptable.html -r --table 'class="dptable"' --tr 'class=tr-collapsed' dp-spreadsheet.csv
-
-echo "document.write('$(cat dptable.html | sed 's/\x27/\&quot;/g' | tr '\n' ' ' )');" > dptable.js
