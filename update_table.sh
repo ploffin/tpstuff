48c6bf4e769212ef94f82ca3bfb64757d752b02d
Ploffin <plofftag@gmail.com>
2017-08-26 01:58:28 +0200

    delete intermediate files when done

--- update_table.sh
+++ update_table.sh
@@ -5,6 +5,7 @@ python get_packet.py
 csv2html -o dptable.html -r --table 'class="dptable"' --tr 'class=tr-collapsed' dp-spreadsheet.csv
 
 echo "document.write('$(cat dptable.html | sed 's/\x27/\&quot;/g' | tr '\n' ' ' )');" > dptable.js
+rm dptable.html
 
 git reset
 git add dptable.js
