aed5c1e3fc75d2056bd7e7288ff0902ac4feccb3
Ploffin <plofftag@gmail.com>
2017-08-29 02:01:47 +0200

    add rowspan and colspan

--- format_packet.py
+++ format_packet.py
@@ -6,18 +6,37 @@ def sanitize(text):
     return unicode(text.lower(),'utf-8').translate({ord(' '): u'-'})
 
 # Get classnames to be added to respective columns
-# of each header cells, and add class attributes
-# to the header cells
-def process_header(table):
-    header = table.find('.//thead')
+# of each header cells, add class attributes and colspan
+# where specified to the header cells. Since colspan
+# creates new cells, we delete the corresponding
+# number of cells to merge
+def process_header(table,merge={},delete={},no_rowspan={}):
     classnames = []
-    for i, cell in enumerate(header.iter('th')):
-        if cell.text != None:
-            text = sanitize(cell.text)
-        else:
+    to_remove = []
+    for i,cell in enumerate(table.iter('th')):
+        if len(to_remove):
+            cell.getparent().remove(cell)
+            to_remove.pop()
             text = 'col-%d' % i
-        classnames.append(text)
-        cell.attrib['class'] = text
+            classnames.append(text)
+            cell.attrib['class'] = text
+        else:
+            if cell.text != None:
+                text = sanitize(cell.text)
+            else:
+                text = 'col-%d' % i
+            classnames.append(text)
+            if text in delete:
+                cell.getparent().remove(cell)
+            else:
+                cell.attrib['class'] = text
+                if text not in no_rowspan:
+                    cell.attrib['rowspan'] = '2'
+                if text in merge:
+                    colspan_val = merge[text]
+                    to_remove = range(int(colspan_val)-1)
+                    cell.attrib['colspan'] = colspan_val
+
     return classnames
 
 # Add class attribute classname to element, admitting
@@ -44,11 +63,18 @@ def add_class_to_element(classname,element,special_classnames={}):
 # Populate table body with class attributes
 def add_classes_to_table(table,classnames,special_classnames={}):
     for i, row in enumerate(table.iter('tr')):
+        cells = row.findall('td')
         if i != 0 and i != 1:
-            cells = row.findall('td')
             for j, cell in enumerate(cells):
                 classname = classnames[j]
                 add_class_to_element(classname,cell,special_classnames)
+        elif i == 1:
+            for j, cell in enumerate(cells):
+                classname = classnames[j]
+                if cell.text != None:
+                    add_class_to_element('%s %s' % (classname,sanitize(cell.text)),cell)
+                else:
+                    cell.getparent().remove(cell)
     return
             
 # Define which content has special classes
@@ -67,10 +93,18 @@ special_classnames = {
         'microphone': mic_case,
         'ping'      : ping_case
         }
+merge_cols = {
+        'tagpro-name':'3',
+        'location':'2',
+        'ping':'2',
+        }
+no_rowspan = {
+        'ping',
+        }
 
 # Read table and add class attributes
 dptable = etree.parse('dptable.html')
-classnames = process_header(dptable)
+classnames = process_header(dptable,merge_cols,{},no_rowspan)
 add_classes_to_table(dptable,classnames,special_classnames)
 
 # Insert table into main document
