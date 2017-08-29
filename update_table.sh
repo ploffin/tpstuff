7c587a4425b0e8c91c2777fe82a1394d68a917d9
Ploffin <plofftag@gmail.com>
2017-08-29 02:01:22 +0200

    remove thead bc it makes rowspan easier

--- update_table.sh
+++ update_table.sh
@@ -4,7 +4,6 @@ rm dptable.html
 rm dptable.csv
 python get_packet.py 
 csv2html -o dptable.html --table 'class="dptable"' --tr 'class="tr-collapsed"' dptable.csv
-sed -i '2 s/\(^.*$\)/<thead>\1<\/thead>/' dptable.html
 python format_packet.py
 
 git reset
